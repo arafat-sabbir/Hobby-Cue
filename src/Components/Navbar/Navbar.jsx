4ff1c068f943f3c5cdf235ff075516cfd82a6056
arafat-sabbir <arafatshabbir8@gmail.com>
2024-03-04 08:50:56 +0600

    Added Navbar And React-icons

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,63 @@
-const Navbar = () => {
-    return (
-        <div className="">
-            <nav className="flex justify-between container mx-auto my-6">
-                <div className=" flex justify-center items-center">
+import { HiOutlineArrowLongRight } from "react-icons/hi2";
+import { SlGlobe } from "react-icons/sl";
 
-                    <img src="https://i.ibb.co/JqPK1nP/785cdcec05510cbdc9cb1b82a17b77fc.png" className="w-72 h-16" alt="" />
-                    <input type="text" name="search" placeholder="Search Here" className="bg-gray-100 px-2 py-2 rounded-tl-xl rounded-bl-xl" />
-                    <button></button>
+const Navbar = () => {
+    const Navlinks =
+        <>
+            <ul className="flex gap-4 ml-4">
+                <li>Product</li>
+                <li>Solutions</li>
+                <li>Resources</li>
+                <li>Enterprise</li>
+                <li>Pricing</li>
+            </ul>
+        </>
+    return (
+        <div className="navbar bg-base-100 px-10">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <a>Parent</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
                 </div>
-                <div>akdjl</div>
-            </nav>
+                <a className="text-xl">miro</a>
+                <div className="">
+                    {Navlinks}
+                </div>
+            </div>
+            {/* <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Item 1</a></li>
+                    <li>
+                        <details>
+                            <summary>Parent</summary>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Item 3</a></li>
+                </ul>
+            </div> */}
+            <div className="navbar-end">
+                <ul className="flex gap-4 mr-4">
+                    <li className="flex items-center gap-1"><SlGlobe size={26} /> en</li>
+                    <li>Contact Sales</li>
+                    <li>Login</li></ul>
+                <a className="px-6 py-4 bg-[#4262FF] rounded-full flex items-center gap-1 text-white font-medium">Sign Up Free <HiOutlineArrowLongRight /></a>
+            </div>
         </div>
     );
 };
